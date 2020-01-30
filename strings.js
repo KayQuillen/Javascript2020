3a3a921df2faee7e8f24df2dc59ba415136f0096
KayQuillen <kaseyrquillen@gmail.com>
2020-01-30 12:28:29 -0600

    created simple website with all resources

---
+++ strings.js
@@ -0,0 +1,8 @@
+function Help(){
+document.getElementById("demo").innerHTML = "Thanks for helping me."
+alert("Thank you for saving me from that chicken!")
+}
+function dontHelp(){
+document.getElementById("demo").innerHTML = "Why won't you help?!?!"
+alert("It's going to peck me!!")
+}
