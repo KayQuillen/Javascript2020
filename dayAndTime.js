b36b09ddd1286c1bd9ad058f66c49c5726d1a2c1
KayQuillen <kaseyrquillen@gmail.com>
2020-01-30 15:49:36 -0600

    created date and time

---
+++ dayAndTime.js
@@ -0,0 +1,19 @@
+// This JS page will be a util for other HTML pages
+
+
+
+//?         var start = new Date()
+//!          console.log(start)
+
+
+function dandt(){
+    var dt = new Date();
+    var today = (dt.getMonth()+1)+"-"+dt.getDate()+"-"+dt.getFullYear()
+
+console.log(today)
+var time = dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds()
+console.log(time)
+document.getElementById("time").innerHTML = today + " " + time;
+
+}
+
