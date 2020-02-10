eaf9770d2ccc038d834287eedc0a01a212ccbab9
KayQuillen <kaseyrquillen@gmail.com>
2020-02-10 13:37:41 -0600

    new code js files

---
+++ arithmatic.js
@@ -0,0 +1,24 @@
+var num1 = 8;
+var num2 = 3;
+// addition
+console.log(
+"Addition Operator : " + (num1 + num2)) ;
+// subtraction
+console.log("Subtraction Operator : " + (num1 - num2));
+// multiplication
+console.log(" Number 1 is equal to: " + num1 + " Number 2 is equal to: " + num2 + " The result of " + num1 + " * " + num2 + " is: " + (num1 * num2))
+// Division
+console.log(" Number 1 is equal to: " + num1 + " Number 2 is equal to: " + num2 + " The result of " + num1 + " / " + num2 + " is: " + (num1 / num2))
+//modulus, larger number must come first
+console.log(" Number 1 is equal to: " + num1 + " Number 2 is equal to: " + num2 + " The result of " + num1 + " % " + num2 + " is: " + (num1 % num2))
+// increment
+var total = ++num1
+console.log(total)
+// for loop
+for(i=0;i<=10;i++){
+    console.log(i)
+}
+//Decrement
+var number = --num2
+console.log(number)
+console.log(--num2)
---
+++ relational.js
@@ -0,0 +1,22 @@
+// equal to
+// All operators for relational result in true or false statements
+var A = 1 ;
+var B = "one";
+var C = "1";
+var D = 2 ;
+console.log(A==B);
+console.log(A==C)
+// not equal to
+console.log(A!=B)
+//greater
+console.log(A>D);
+console.log(A>C)
+// Less than
+console.log(A<D)
+//greater than or equal to
+console.log(A>=C)
+//less than or equal to
+console.log(D<=A)
+
+
+
