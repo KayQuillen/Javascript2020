5cd1d13f814871eebac09c257e5599eedeacc741
KayQuillen <kaseyrquillen@gmail.com>
2020-02-12 15:59:35 -0600

    bubble solution test

---
+++ typeOf/typeOf.js
@@ -0,0 +1,16 @@
+var a,b,c,d,e;
+a=1
+b="hi"
+c={}
+d=true
+e = "567"
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(a.toString());
+console.log(d.toString())
+console.log(Date.now().toString())
+console.log(Number(b))
+console.log(Number(d))
+console.log(Number(e))
